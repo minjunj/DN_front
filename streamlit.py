d86f2708591dc6456bfe9352329b9b59571a716b
minjunj <minjun_jo@gm.gist.ac.kr>
2024-04-28 18:45:19 +0900

    update : add intro png

--- streamlit.py
+++ streamlit.py
@@ -8,6 +8,9 @@ from st_pages import Page, Section, show_pages, add_page_title
 
 st.title('''Welcome Group 4's World!''')
 
+
+# Display the image, which should be centered with the CSS above
+st.image('asset/intro.png', width=500)
 show_pages(
     [
         Page("streamlit.py", "Main Introduce", "💡"),
