a1d0426ec512a6572aa2bb7c9dfaf34324bbd3b1
minjunj <minjun_jo@gm.gist.ac.kr>
2024-04-28 15:17:30 +0900

    craete : sett initial from

---
+++ other_pages/writting.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from st_pages import Page, Section, show_pages, add_page_title
+
+
+st.title('Writting a Article!')
--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from st_pages import Page, Section, show_pages, add_page_title
 
-st.title('Uber pickups in NYC')
+#side bar part refer https://github.com/blackary/st_pages
+# add_page_title()
+
+st.title('''Welcome Group 4's World!''')
+
+show_pages(
+    [
+        Page("streamlit.py", "Main Introduce", "💡"),
+        Page("other_pages/writting.py", "Writting a Article!", "📝"),
+        Page("other_pages/trend_predict.py", "Predict Article's View!!", "🔥"),
+        # Unless you explicitly say in_section=False
+    ]
+)
